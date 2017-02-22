c24926d0feeeb965287d4c213fdcf3730cd5bfee
tlumberg <tlumberg@vcd-ceb131-24.eastern.ewu.edu>
2017-02-22 09:32:04 -0800

    Randomized Image
    *Random unsplash image
    *template for project

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-// YOUR SCRIPTS GO HERE
+//Randomize Image
+
+//Store Image URLs
+
+var img0 = '../img/200.jpg';
+var img1 = '../img/300.jpg';
+var img2 = '../img/400.jpg';
+
+
+
+//Get Random Number
+var max = 3;
+var numRand = Math.floor( Math.random() * max );
+console.log(numRand);
+
+//Show Images
+$('img').attr('src', eval('img' + numRand) ); 
