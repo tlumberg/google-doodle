6387b30a79c9ce3edc6a5efbeb05c369424fab7e
tlumberg <timlumberg@hotmail.com>
2017-03-02 20:29:56 -0800

    added new doodle/styled

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,10 @@
-//Randomize Image
-
-//Store Image URLs
-
-var img0 = '../img/200.jpg';
-var img1 = '../img/300.jpg';
-var img2 = '../img/400.jpg';
-
-
-
-//Get Random Number
-var max = 3;
-var numRand = Math.floor( Math.random() * max );
-console.log(numRand);
-
-//Show Images
-//$('img').attr('src', eval('img' + numRand) ); 
-
-$('#stage svg').hide();
-
-//click on button
-
-$('button').click(function() {
-    
-    
-    //hide circle
-    
-    
-    //show circle
-    $('#stage svg').show();
-    
+// Store SVG points
+var google = $('svg').drawsvg({
+  duration : 4000,
+  reverse: true,
+  stagger: 1000
 });
+
+
+// Animate SVG points
+google.drawsvg('animate');
