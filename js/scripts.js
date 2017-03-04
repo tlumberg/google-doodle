6d0573f48aa1c7b2ff9d4dc2a9ba01cf304bbcdc
tlumberg <timlumberg@hotmail.com>
2017-03-03 17:36:50 -0800

    Changed timing on Background

--- js/scripts.js
+++ js/scripts.js
@@ -16,4 +16,4 @@ google.drawsvg('animate');
       "http://www.cristabel-darryl.com/wp-content/uploads/2016/01/Gatsby-ArtDeco-Background.png"
     , "https://s-media-cache-ak0.pinimg.com/originals/7f/8a/6c/7f8a6c89c4242f10fdcd176cae973a33.jpg"
     , "https://i.ytimg.com/vi/FdMX-537FBg/maxresdefault.jpg"
-  ], {duration: 2000, fade: 750});
+  ], {duration: 3000, fade: 750});
